a141f56c6a5f03a60ae828841062c72807cf7b23
Ziyad Edher <ziyad.edher@gmail.com>
2022-01-25 22:31:17 -0800

    Fix hex type in stories

--- packages/dao-interface-webapp/src/utils/hex.ts
+++ packages/dao-interface-webapp/src/utils/hex.ts
@@ -1,7 +1,10 @@
-type HexType = "address" | "tx";
+const HEX_TYPES = ["address", "tx"] as const;
+
+type HexType = typeof HEX_TYPES[number];
 
 const getEtherscanLink = (address: string, type: HexType): string =>
   `https://rinkeby.etherscan.io/${type}/${address}`;
 
+export { HEX_TYPES };
 export type { HexType };
 export { getEtherscanLink };
--- packages/dao-interface-webapp/stories/components/links/hex_link.stories.tsx
+++ packages/dao-interface-webapp/stories/components/links/hex_link.stories.tsx
@@ -1,5 +1,7 @@
 import HexLink from "../../../src/components/links/hex_link";
+import { HEX_TYPES } from "../../../src/utils/hex";
 
+import type { HexType } from "../../../src/utils/hex";
 import type { Meta, Story } from "@storybook/react";
 
 const META: Meta = {
@@ -8,22 +10,33 @@ const META: Meta = {
 };
 
 interface HexLinkTemplateProps {
+  readonly type: HexType;
   readonly hex: string;
   readonly maxLength: number;
 }
 
 const HexLinkTemplate: Story<HexLinkTemplateProps> = ({
+  type,
   hex,
   maxLength,
-}: HexLinkTemplateProps) => <HexLink hex={hex} maxLength={maxLength} />;
+}: HexLinkTemplateProps) => (
+  <HexLink type={type} hex={hex} maxLength={maxLength} />
+);
 
 const HexLinkStory = HexLinkTemplate.bind({});
 HexLinkStory.storyName = "Hex Link";
 HexLinkStory.args = {
+  type: "address",
   hex: "0xd11fA2FcFE6e21184b496c20f299Ce4B3722C737",
   maxLength: 4,
 };
 HexLinkStory.argTypes = {
+  type: {
+    control: {
+      type: "select",
+      options: HEX_TYPES,
+    },
+  },
   maxLength: {
     control: { type: "range", min: 0, max: 64, step: 1 },
   },
